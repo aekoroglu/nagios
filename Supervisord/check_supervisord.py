5ca80677c3cc2848f71f36280fbdec641861284f
Ali Erdinç Köroğlu <aekoroglu@spp42.com>
2020-04-12 20:00:38 +0300

    new version with alerts and python upgrade

--- Supervisord/check_supervisord.py
+++ Supervisord/check_supervisord.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+
 #
-# Supervisord plugin for Nagios by Ali Erdinc Koroglu - http://ae.koroglu.org
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Supervisord plugin for Nagios by Ali Erdinc Koroglu
+# 
+# Licensed under the GNU General Public License v3.0
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+#
 
-import xmlrpclib
-import socket
-from socket import error as socket_error
-import datetime
-from dateutil.relativedelta import relativedelta
-from optparse import OptionParser
+from xmlrpc.client import ServerProxy
+import optparse
 import sys
-
-usage = "usage: %prog -H 192.168.1.1 -P 9001 -u superv -p superv -a glassfish"
-parser = OptionParser(usage=usage)
-parser.add_option('-H', '--hostname', dest='hostname', help="Supervisord hostname")
-parser.add_option('-P', '--port', dest='port', help="Supervisord port")
-parser.add_option('-u', '--username', dest='username', help="Supervisord username")
-parser.add_option('-p', '--password', dest='password', help="Supervisord password")
-parser.add_option('-a', '--process-name', dest='procname', help="Process name defined in /etc/supervisor.d/*.ini or supervisorctl status")
-(opts, args) = parser.parse_args()
 
 def superv_state(state):
     if state == 'RUNNING':
@@ -40,48 +29,95 @@ def superv_state(state):
     else:
         return 'UNKNOWN'
 
-def time_diff_text(diff):
-    if diff.days >= 1:
-        return '%s day(s) %s hour(s) %s minute(s)' % (diff.days, diff.hours, diff.minutes)
-    elif diff.hours > 1:
-        return '%s hour(s) %s minute(s)' % (diff.hours, diff.minutes)
-    else:
-        return '%s minute(s)' % (diff.minutes)
 
-def proper_exit(status):
-    if status == 'OK':
+def get_state(process, state, desc, now, start, warning, critical, time):
+    tdiff = now - start
+    if time == 'minute':
+        proc_upt = round(tdiff/60,2)
+    elif time == 'hour':
+        proc_upt = round(tdiff/3600,2)
+    elif time == 'day':
+        proc_upt = round(tdiff/86400,2)
+    else:
+        proc_upt = tdiff
+
+    if warning is None and critical is None:
+        print ('%s %s: %s | uptime=%s;;;' % (process, state, desc, proc_upt))
         sys.exit(0)
-    elif status == 'WARNING':
-        sys.exit(1)
-    elif status == 'CRITICAL':
-        sys.exit(2)
-    else:
-        sys.exit(3)
-
-def superv_status(hostname,port,username,password,procname):
-    try:
-        if hostname is None or port is None:
-            parser.print_help()
-            sys.exit()
-        if username is None and password is None:
-            serverurl = "http://%s:%s" % (hostname,port)
+    elif critical is None:
+        if proc_upt <= warning:
+            print('%s WARNING : %s | uptime=%s;%s;;' % (process, desc, proc_upt, warning))
+            sys.exit(1)
         else:
-            serverurl = "http://%s:%s@%s:%s" % (username,password,hostname,port)
-        socket.setdefaulttimeout(10)
-        server = xmlrpclib.Server(serverurl)
-        info = server.supervisor.getProcessInfo(procname)
-        sprv_status = superv_state(info['statename'])
-        time1 = datetime.datetime.fromtimestamp(info['start'])
-        time2 = datetime.datetime.fromtimestamp(info['now'])
-        diff = relativedelta(time2,time1)
-        uptime_text = time_diff_text(diff)
-        print '%s %s: %s' % (procname, sprv_status, uptime_text)
-        proper_exit(sprv_status)
-    except (socket_error,xmlrpclib.ProtocolError,xmlrpclib.ResponseError), error_code:
-        print "CRITICAL: Could not connect to supervisord %s" % error_code
-        sys.exit(2)
-    except Exception, error_code:
-        print "CRITICAL: %s" % error_code
+            print ('%s %s: %s | uptime=%s;%s;;' % (process, state, desc, proc_upt, warning))
+            sys.exit(0)
+    elif warning is None:
+        if proc_upt < critical:
+            print('%s CRITICAL : %s | uptime=%s;;%s;' % (process, desc, proc_upt, critical))
+            sys.exit(2)
+        else:
+            print ('%s %s: %s | uptime=%s;;%s;' % (process, state, desc, proc_upt, critical))
+            sys.exit(0)
+    else:
+        if warning <= critical:
+            print ('CRITICAL : Warning value can not be less than or equal to critical')
+            sys.exit(2)
+        else:
+            if proc_upt < critical:
+                print('%s CRITICAL : %s | uptime=%s;%s;%s;' % (process, desc, proc_upt, warning, critical))
+                sys.exit(2)
+            elif proc_upt <= warning:
+                print('%s WARNING : %s | uptime=%s;%s;%s;' % (process, desc, proc_upt, warning, critical))
+                sys.exit(1)
+            else:
+                print ('%s %s: %s | uptime=%s;%s;%s;' % (process, state, desc, proc_upt, warning, critical))
+                sys.exit(0)
+
+
+def get_info(server,port,username,password,process,warning,critical,time):
+    try:
+        if server is None and port is None:
+            print ('Server and port is required')
+            sys.exit(2)
+        elif process is None:
+            print ('Process is required')
+            sys.exit(2)
+        
+        if username is None and password is None:
+            uri = 'http://%s:%s/RPC2' % (server,port)
+        else:
+            uri = 'http://%s:%s@%s:%s/RPC2' % (username,password,server,port)
+        
+        server = ServerProxy(uri)
+        proc_info = server.supervisor.getProcessInfo(process)
+        proc_state = superv_state(proc_info['statename'])
+        proc_desc = proc_info['description']
+        proc_now = proc_info['now']
+        proc_start = proc_info['start']
+        get_state(process, proc_state, proc_desc, proc_now, proc_start, warning, critical, time)
+    except Exception as e:
+        print('CRITICAL : %s' % e)
         sys.exit(2)
 
-superv_status(opts.hostname,opts.port,opts.username,opts.password,opts.procname)
+
+def help_parser():
+    parser = optparse.OptionParser(usage="usage: %prog -s 192.168.1.1 -P 9001 -u superv -p superv -a tomcat", version="%prog 2.0")
+    parser.add_option('-s', '--server', action='store', type='string', dest='server', default=None, help='IP address or hostname you want to connect')
+    parser.add_option('-P', '--port', action='store', type='int', dest='port', default=None, help="TCP port")
+    parser.add_option('-u', '--username', action='store', type='string', dest='username', default=None, help="Username")
+    parser.add_option('-p', '--password', action='store', type='string', dest='password', default=None, help="Password")
+    parser.add_option('-a', '--process-name', action='store', type='string', dest='process', default=None, help="Process name")
+    parser.add_option('-t', '--time', action='store', type='string', dest='time', default='minute', help="Time unit of measurement (second|minute|hour|day) (default: minutes)")
+    parser.add_option('-w', '--warning', action='store', type='int', dest='warning', default=None, help='The warning threshold')
+    parser.add_option('-c', '--critical', action='store', type='int', dest='critical', default=None, help='The critical threshold')
+    return parser
+
+
+def main():
+    parser = help_parser()
+    options, _args = parser.parse_args()
+    get_info(options.server,options.port,options.username,options.password,options.process,options.warning,options.critical,options.time)
+
+
+if __name__ == "__main__":
+    main()
